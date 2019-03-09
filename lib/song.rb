1f0cb022b442140d67f708786a9c99a7d87eb1c8
David Forer <david.j.forer@gmail.com>
2019-03-09 18:11:56 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song 
+  attr_accessor :name, :artist 
+  
+  @@all = []
+  
+  def initalize(name)
+    @name = name 
+    @@all = self
+    
+  end
+  
+  def self.all 
+    @@all
+    
+  end
+  
+end
